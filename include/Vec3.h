3fcf2a4183bcf4c9f5a5f112c3c4fc68f807f1fc
ProbablyAarav <aarav25803691@gmail.com>
2026-07-20 20:00:20 -0500

    vec3 declaration

--- include/Vec3.h
+++
@@ -1,6 +0,0 @@
-template<typename T>
-struct Vec3 {
-  T x, y, z;
-  Vec3();
-  Vec3(T x, T y, T z);
-};
---
+++ include/vec3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+template<typename T>
+struct Vec3 {
+  T x, y, z;
+  Vec3();
+  Vec3(T x, T y, T z);
+};
+template<typename T>
+Vec3<T>::Vec3(): x(0), y(0), z(0) {}
+template<typename T>
+Vec3<T>::Vec3(T x, T y, T z): x(x), y(y), z(z) {}
+
