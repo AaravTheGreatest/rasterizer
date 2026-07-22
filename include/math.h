71771cbc25baacc7517cd19ad810aebf9d5936a7
ProbablyAarav <aarav25803691@gmail.com>
2026-07-21 21:01:11 -0500

    Easter egg :D

--- include/math.h
+++ include/math.h
@@ -2,3 +2,6 @@
 
 #include <math/typedefs.h>
 
+namespace raster {
+    using CppFrFr = Vec2f; // lil Easter egg because GitHub said this is a C file when the 2 lines I had in here before were CLEARLY C++ trust /s
+} // please stop accusing my poor C++ header of being C o great GitHub indexer
